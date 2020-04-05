970e56985e99c7e9bac4ec3f866d5c3ccafd563f
feiniao2000 <feiniao2000@yuanmei.com>
2020-04-05 18:20:17 +0800

    add vector add

--- opencl_demo/jni/ImageSobelFilter.cpp
+++ opencl_demo/jni/ImageSobelFilter.cpp
@@ -112,30 +112,8 @@ void dumpCLInfo()
 			LOGD( "OpenCL info: Device[%d] = %s (%s) \n\t ext = %s",
 				  i, name, (type==CL_DEVICE_TYPE_GPU ? "GPU" : "CPU"), extensions);
         }
-        //		{
-//			std::string name = devices[i].getInfo<CL_DEVICE_NAME>();
-//			std::string extensions = devices[i].getInfo<CL_DEVICE_EXTENSIONS>();
-//			cl_ulong type = devices[i].getInfo<CL_DEVICE_TYPE>();
-//			LOGD( "OpenCL info: Device[%d] = %s (%s), ext = %s",
-//				  i, name.c_str(), (type==CL_DEVICE_TYPE_GPU ? "GPU" : "CPU"), extensions.c_str() );
-//		}
 
     }
-
-
-
-
-//		std::vector<cl::Device> devices;
-//		platforms[0].getDevices(CL_DEVICE_TYPE_ALL, &devices);
-//
-//		for (int i = 0; i < devices.size(); ++i)
-//		{
-//			std::string name = devices[i].getInfo<CL_DEVICE_NAME>();
-//			std::string extensions = devices[i].getInfo<CL_DEVICE_EXTENSIONS>();
-//			cl_ulong type = devices[i].getInfo<CL_DEVICE_TYPE>();
-//			LOGD( "OpenCL info: Device[%d] = %s (%s), ext = %s",
-//				  i, name.c_str(), (type==CL_DEVICE_TYPE_GPU ? "GPU" : "CPU"), extensions.c_str() );
-//		}
 
 	LOGD("*******************");
 }
@@ -147,7 +125,154 @@ void initOpenCL() {
 	dumpCLInfo();
 }
 
-void cpu_process(void) {
+const char *kernel_vadd =
+        "__kernel void vecAdd(  __global int *a,\n"
+        "                       __global int *b,\n"
+        "                       __global int *c,\n"
+        "                       const unsigned int n)\n"
+        "{\n"
+        "    int id = get_global_id(0);\n"
+        "    c[id] = a[id] + b[id];\n"
+        "}\n";
+
+void VectorAddByGpu(void) {
+
+    clock_t start_t, end_t;
+
+
+    // Length of vectors
+    unsigned int n = 147456;
+
+    // Host input vectors
+    int *h_a;
+    int *h_b;
+    // Host output vector
+    int *h_c;
+
+    // Device input buffers
+    cl_mem d_a;
+    cl_mem d_b;
+    // Device output buffer
+    cl_mem d_c;
+
+    cl_platform_id cpPlatform;        // OpenCL platform
+    cl_device_id device_id;           // device ID
+    cl_context context;               // context
+    cl_command_queue queue;           // command queue
+    cl_program program;               // program
+    cl_kernel kernel;                 // kernel
+
+    // Size, in bytes, of each vector
+    size_t bytes = n * sizeof(int);
+
+    // Allocate memory for each vector on host
+    h_a = (int *) malloc(bytes);
+    h_b = (int *) malloc(bytes);
+    h_c = (int *) malloc(bytes);
+
+    // Initialize vectors on host
+    int i;
+    for (i = 0; i < n; i++) {
+        h_a[i] = i;
+        h_b[i] = i + i;
+        h_c[i] = 0;
+    }
+
+    size_t globalSize, localSize;
+    cl_int err;
+    // Number of work items in each local work group
+    localSize = 64;
+
+    // Number of total work items - localSize must be devisor
+    globalSize = n;
+
+    // Bind to platform
+    err = clGetPlatformIDs(1, &cpPlatform, NULL);
+
+    // Get ID for the device
+    err = clGetDeviceIDs(cpPlatform, CL_DEVICE_TYPE_GPU, 1, &device_id, NULL);
+
+    // Create a context
+    context = clCreateContext(0, 1, &device_id, NULL, NULL, &err);
+
+    // Create a command queue
+    queue = clCreateCommandQueue(context, device_id, 0, &err);
+
+    // Create the compute program from the source buffer
+    program = clCreateProgramWithSource(context, 1,
+                                        (const char **) &kernel_vadd, NULL, &err);
+
+    // Build the program executable
+    clBuildProgram(program, 0, NULL, NULL, NULL, NULL);
+
+    // Create the compute kernel in the program we wish to run
+    kernel = clCreateKernel(program, "vecAdd", &err);
+
+    start_t = clock(); // Start Time!
+    // Create the input and output arrays in device memory for our calculation
+    d_a = clCreateBuffer(context, CL_MEM_READ_ONLY|CL_MEM_COPY_HOST_PTR, bytes, h_a, NULL);
+    d_b = clCreateBuffer(context, CL_MEM_READ_ONLY|CL_MEM_COPY_HOST_PTR, bytes, h_b, NULL);
+    d_c = clCreateBuffer(context, CL_MEM_WRITE_ONLY, bytes, NULL, NULL);
+
+
+    // Set the arguments to our compute kernel
+    err = clSetKernelArg(kernel, 0, sizeof(cl_mem), &d_a);
+    err |= clSetKernelArg(kernel, 1, sizeof(cl_mem), &d_b);
+    err |= clSetKernelArg(kernel, 2, sizeof(cl_mem), &d_c);
+    err |= clSetKernelArg(kernel, 3, sizeof(unsigned int), &n);
+
+    double k1 = clock();
+    // Execute the kernel over the entire range of the data set
+    err = clEnqueueNDRangeKernel(queue, kernel, 1, NULL, &globalSize, &localSize,
+                                 0, NULL, NULL);
+    double k2 = clock();
+
+    // Wait for the command queue to get serviced before reading back results
+//    clFinish(queue);
+
+    // Read the results from the device
+    clEnqueueReadBuffer(queue, d_c, CL_TRUE, 0,
+                        bytes, h_c, 0, NULL, NULL);
+
+
+    // Ends timing since rest is validation and cleanup
+    end_t = clock();
+    double time_t = (double)(end_t - start_t) / CLOCKS_PER_SEC;
+    LOGD("Gpu Vector Add - used time: %f s , kernel use time:%f s", time_t, (k2 - k1)/CLOCKS_PER_SEC);
+
+    for (i = 0; i < n; i++) {
+        if(h_c[i] != (h_a[i] + h_b[i])) {
+            LOGD("Vector add err at i=%d c=%d a=%d b=%d", i, h_c[i], h_a[i], h_b[i]);
+            break;
+        }
+    }
+
+    if (i >= n) {
+        LOGD("Vector Add - success");
+    }
+
+
+    start_t = clock(); // Start Time!
+    for (i = 0; i < n; i++) {
+        h_c[i] = h_a[i] + h_b[i];
+    }
+    end_t = clock();
+    time_t = (double)(end_t - start_t) / CLOCKS_PER_SEC;
+    LOGD("Cpu Vector Add - used time: %f s", time_t);
+
+    // release OpenCL resources
+    clReleaseMemObject(d_a);
+    clReleaseMemObject(d_b);
+    clReleaseMemObject(d_c);
+    clReleaseProgram(program);
+    clReleaseKernel(kernel);
+    clReleaseCommandQueue(queue);
+    clReleaseContext(context);
+
+    //release host memory
+    free(h_a);
+    free(h_b);
+    free(h_c);
 }
 
 extern "C" {
@@ -157,34 +282,9 @@ JNIEXPORT jstring JNICALL Java_com_white_imagesobelfilter_nativeSobelFilter_sobe
 
 	initOpenCL();
 
-
-	clock_t start, finish;
-	double CPU_time, GPU_time;
-
-	//GPU
-	start = clock();	//gpu
-
-	finish = clock();	//gpu
-	GPU_time = (double) (finish - start) / CLOCKS_PER_SEC;
-
-	//CPU
-	start = clock();
-	cpu_process();
-	finish = clock();	//cpu
-	CPU_time = (double) (finish - start) / CLOCKS_PER_SEC;
-
-	double s = CPU_time / GPU_time;	//
-
-	double f = 0.0;
-
+    VectorAddByGpu();
 
 	char rr[100] = "Compute result:\nGPU:";
-	char b[10];
-	sprintf(b, "%.8f", GPU_time);
-	sprintf(b, "%.8f", CPU_time);
-
-	sprintf(b, "%.8f", s);
-	sprintf(b, "%.8f", f);
 
 
 	const char* result = rr;
